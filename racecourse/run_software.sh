5099454b1902606cb579bc6e8e8bddc055219130
arjoonn <arjoonn@midpathsoftware.com>
2025-10-31 09:53:54 +0530

    x

---
+++ racecourse/run_software.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+main(){
+	IMAGE=baseline
+	read -p "Enter docker image [default: $IMAGE]: " IMAGE
+	export IMAGE
+
+	IDENTIFIER=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13; echo)
+	read -p "Enter identifier [default: $IDENTIFIER]: " IDENTIFIER
+	export IDENTIFIER
+
+	docker run --it --restart always --cpu 1 --memory 500m --label is_racer=y --network racecourse --network-alias "player_$IDENTIFIER" --name "player_$IDENTIFIER" "$IMAGE"
+}
+(main)
